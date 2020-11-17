57f91620fd514a202640a980d3c858701dfbecda
hghanbari <70950300+hghanbari@users.noreply.github.com>
2020-11-17 18:24:39 +0100

    Initial commit

---
+++ h01/MijnJS.js
@@ -0,0 +1,17 @@
+console.log(8+9)
+var b=10;
+
+if (b>9){
+    console.log("Halloo world")
+}
+
+for( var i=0 ; i<10 ; i++){
+    console.log("De waarden van is " + i)
+}
+var mijnauto ={
+    merk:"BMW",
+    type: "Seri3",
+    aantalWielen: 4,
+    kleur: "Blauw",
+}
+console.log(mijnauto.type)
