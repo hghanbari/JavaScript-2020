97245ea299cf0867653d2bf32f071ce30779fd1e
hghanbari <70950300+hghanbari@users.noreply.github.com>
2020-11-19 10:19:08 +0100

    Hoofdstuk2 opdracht1

---
+++ h02/Color.js
@@ -0,0 +1,31 @@
+
+document.getElementById("body").style.padding ="0";
+document.getElementById("body").style.margin ="0";
+document.getElementById("body").style.backgroundColor ="aliceblue";
+
+document.getElementById("b1").style.backgroundColor = "#59cce7";
+document.getElementById("b2").style.backgroundColor ="#4cd611";
+document.getElementById("b3").style.backgroundColor = "#efca4c";
+document.getElementById("b4").style.backgroundColor = "#d61148";
+
+document.getElementById("b1").style.padding = "20px";
+document.getElementById("b2").style.marginTop= "10px";
+document.getElementById("b3").style.marginTop= "40px";
+document.getElementById("b3").style.textAlign = "Right";
+document.getElementById("b2").style.textAlign = "center";
+
+document.getElementById("b1").style.float = "left";
+document.getElementById("b2").style.float = "left";
+document.getElementById("b3").style.float = "left";
+document.getElementById("b4").style.float = "left";
+
+document.getElementById("b4").style.boxSizing = "border-box";
+document.getElementById("b3").style.boxSizing = "border-box";
+document.getElementById("b2").style.boxSizing = "border-box";
+document.getElementById("b1").style.boxSizing = "border-box";
+
+
+
+
+
+
