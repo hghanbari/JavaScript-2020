8ba828ace16dec77a11cc49935658a458aeb7c31
H . G <gh.hamid89@gmail.com>
2021-01-19 22:27:03 +0100

    Hoofdstuk 6 klaar

--- h06/Memory.js
+++ h06/Memory.js
@@ -3,7 +3,7 @@ gamePlast = document.getElementById("gamePlast");
 var numberOfCards = 18;
 createHolder();
 createFrontHolder();
-timedCount();
+
 
 function createHolder() {
     for (var i = 0; i < numberOfCards; i++) {
@@ -15,75 +15,116 @@ function createHolder() {
 
 function createFrontHolder() {
     holder = document.getElementsByClassName("holder");
-    var imgIndex = 1;
-    for (var i = 0; i < holder.length; i++) {
-        var picFront = createImage("img/p" + imgIndex + ".jpg");
-        imgIndex++;
-        if (imgIndex > (numberOfCards / 2)) {
-            imgIndex = 1;
+
+    var random = 0;
+    var randomNum1 = [];
+    var randomNum2 = [];
+    while (randomNum1.length < 9) {
+        random = Math.floor(Math.random() * 9) + 1;
+        if (randomNum1.lastIndexOf(random) === -1) {
+            randomNum1.push(random);
         }
+    }
+    while (randomNum2.length < 9) {
+        random = Math.floor(Math.random() * 9) + 1;
+        if (randomNum2.lastIndexOf(random) === -1) {
+            randomNum2.push(random);
+        }
+    }
+    for (var i = 0; i < randomNum1.length; i++) {
+        var picFront = createImage("img/p" + randomNum1[i] + ".jpg");
         holder[i].appendChild(picFront);
     }
-
-}
-
-function createImage(realImageAddress) {
-    var img = document.createElement("img");
-    img.className = "picFront";
-    img.src = "img/none.jpg";
-    img.dataset.realImg = realImageAddress;
-
-    img.addEventListener('click', function () {
-        flipCard(img);
-    });
-
-    return img;
-}
-
-
-var previousImg = null;
-var round = 0;
-
-function flipCard(img) {
-    if (img === previousImg || img.dataset.done) {
-        return;
+    for (var i = 0; i < randomNum2.length; i++) {
+        var picFront = createImage("img/p" + randomNum2[i] + ".jpg");
+        holder[i + 9].appendChild(picFront);
     }
-    if (previousImg === null) {
-        previousImg = img;
-    } else {
-        if (previousImg.dataset.realImg !== img.dataset.realImg) {
-            // When the player chooses an incorrect card
-            previousImg.src = 'img/none.jpg'
-            previousImg = null;
+
+    function createImage(realImageAddress) {
+        var img = document.createElement("img");
+        img.className = "picFront";
+        img.src = "img/none.jpg";
+        img.dataset.realImg = realImageAddress;
+
+        img.addEventListener('click', function () {
+            flipCard(img);
+        });
+
+        return img;
+    }
+
+    var player1 = {
+        name: 'Player1',
+        points: 0
+    };
+    var player2 = {
+        name: 'Player2',
+        points: 0
+    };
+    var previousImg = null;
+    var round = 0;
+    var Changeplayer = 1;
+
+    var currentPlayer = null;
+
+    function setCurrentPlayer(player) {
+        currentPlayer = player;
+        document.getElementById("current-player").innerText = player.name;
+    }
+
+    var wait = false;
+
+    function flipCard(img) {
+        if (img === previousImg || img.dataset.done || wait) {
             return;
         }
 
-        // When the player choose correct cards
-        previousImg.dataset.done = true;
-        img.dataset.done = true;
-        round++;
-        previousImg = null;
+        if (previousImg === null) {
+            previousImg = img;
+        } else {
+            if (previousImg.dataset.realImg !== img.dataset.realImg) {
+                // When the player chooses an incorrect card
+                wait = true;
+                if (currentPlayer === player1) {
+                    setCurrentPlayer(player2);
+                } else {
+                    setCurrentPlayer(player1);
+                }
 
-        if (round === 9) {
-            alert("You Won!")
-            startCount()
+                img.src = img.dataset.realImg;
+
+                setTimeout(function () {
+                    img.src = 'img/none.jpg';
+                    previousImg.src = 'img/none.jpg';
+                    previousImg = null;
+                    wait = false;
+                }, 1000);
+                return;
+            }
+
+            // When the player choose correct cards
+
+            previousImg.dataset.done = true;
+            img.dataset.done = true;
+            round++;
+            previousImg = null;
+            currentPlayer.points++;
+            if (currentPlayer === player1) {
+                setCurrentPlayer(player2);
+            } else {
+                setCurrentPlayer(player1);
+            }
+
+            if (round === 9) {
+                alert("End of the game!")
+
+            }
         }
-    }
-    img.src = img.dataset.realImg;
-}
+        img.src = img.dataset.realImg;
 
-var c = 0;
-var t;
-var timer_is_on = 0;
-
-function timedCount() {
-    document.getElementById("txt").value = c;
-    c = c + 1;
-    t = setTimeout(timedCount, 1000);
-}
-function startCount() {
-    if (!timer_is_on) {
-        timer_is_on = 1;
-        timedCount();
+        document.getElementById("player1").value = player1.points;
+        document.getElementById("player2").value = player2.points;
     }
+
+    setCurrentPlayer(player1);
 }
